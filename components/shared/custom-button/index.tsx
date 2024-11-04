2e818f5b9bdaa3a42fe685115414dd4d8036b366
Rami-0 <ramysh11@ICLOUD.COM>
2024-11-05 03:32:51 +0600

    initial template

---
+++ components/shared/custom-button/index.tsx
@@ -0,0 +1,7 @@
+interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export default function CustomButton({ children, ...props }: IProps) {
+  return <button {...props}>{children}</button>;
+}
